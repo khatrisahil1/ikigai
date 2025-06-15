4bcb8be3b78f2f3a599a9837fb898209a48545f9
SAHIL KHATRI <khatrisahil821@gmail.com>
2025-06-15 13:27:57 +0530

    Initial Commit

---
+++ Ikigai/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Ikigai
+//
+//  Created by SAHIL KHATRI on 15/06/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Ikigai/IkigaiApp.swift
@@ -0,0 +1,17 @@
+//
+//  IkigaiApp.swift
+//  Ikigai
+//
+//  Created by SAHIL KHATRI on 15/06/25.
+//
+
+import SwiftUI
+
+@main
+struct IkigaiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
